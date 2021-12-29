5fffcd8f35d72b1eeb225eb03093966226a9b55c
J7 <J7DevWeb@gmail.com>
2021-12-29 10:47:45 +0100

    components and routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { CommentsComponent } from './pages/comments/comments.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchMoviesComponent } from './pages/search-movies/search-movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'search', component: SearchMoviesComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'comment', component: CommentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchMoviesComponent } from './pages/search-movies/search-movies.component';
+import { CommentsComponent } from './pages/comments/comments.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
+import { CommentCardComponent } from './components/comment-card/comment-card.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchMoviesComponent,
+    CommentsComponent,
+    AboutComponent,
+    MovieCardComponent,
+    CommentCardComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule,
